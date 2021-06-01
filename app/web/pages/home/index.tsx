8a4731568901e59b8fdf275f69775243c76a8d28
张玉山 <iamyushan@dingtalk.com>
2021-06-01 18:28:31 +0800

    feat: 新增直接使用 request 示例

--- app/web/pages/home/index.tsx
+++ app/web/pages/home/index.tsx
@@ -5,6 +5,11 @@ import { homeService } from '@/services';
 import styles from './index.less';
 import UseComponents from '@/components/useComponents';
 
+export interface Info {
+  fans_token: string;
+  http_request_type: number;
+}
+
 const Home: React.FC<{ unknown: any }> = () => {
   const TYPE = 1
   const TOKEN = '7573893fr38ovdem51q8vkrqjb5t8l24'
@@ -38,6 +43,17 @@ const Home: React.FC<{ unknown: any }> = () => {
   //   }
   // });
 
+  // const run = async (info: Info) => {
+  //   try {
+  //     const { data: { user_info: { nickname }} } = await homeService.getUserInfo(info);
+  //     setMessage(nickname);
+  //     Toast.info(nickname);
+  //     setTimeout(() => {
+  //       history.push('/useDva');
+  //     }, 1000);
+  //   } catch (error) {}
+  // }
+  
   return (
     <div className='sou-main-color' onClick={() => run(info)}>
       <h1 className={styles.h1}>click me to getUserInfo</h1>
